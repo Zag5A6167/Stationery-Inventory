80a3e7d1679ec6737cec99b75b36665d846913a9
z <za1a2ew44zz_999@hotmail.com>
2026-07-14 21:56:49 +0700

    feat(ui): add basic index.html and cors

--- Products.cs
+++ Products.cs
@@ -6,8 +6,12 @@ public class Product
 {
     [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Product name is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
     public string Name { get; set; } = string.Empty;
-
+    [Range(0, 10000, ErrorMessage = "Stock must be between 0 and 10,000")]
     public int Stock { get; set; }
+    [Range(0.01, 1000000, ErrorMessage = "Price must be greater than 0")]
+ 
     public decimal Price { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using StationeryAPI;
 using StationeryAPI.Models;
 using Scalar.AspNetCore;
@@ -11,6 +12,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=stationery.db"));
 
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", (CorsPolicyBuilder builder) =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -19,9 +29,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();   
     app.MapScalarApiReference();
 }
-
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.MapProductEndpoints();
 
 app.Run();
