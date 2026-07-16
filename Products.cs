ce99316b4f7d18f0be1a4a2b9972044460acb81d
z <za1a2ew44zz_999@hotmail.com>
2026-07-16 21:23:36 +0700

    refactor: seperate file style

--- Products.cs
+++ Products.cs
@@ -9,6 +9,7 @@ public class Product
     [Required(ErrorMessage = "Product name is required")]
     [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
     public string Name { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
     [Range(0, 10000, ErrorMessage = "Stock must be between 0 and 10,000")]
     public int Stock { get; set; }
     [Range(0.01, 1000000, ErrorMessage = "Price must be greater than 0")]
